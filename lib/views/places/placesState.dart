e307156ea563d902fa0e829c0ca7274a7f396f2f
Rémy Raes <raes.remy@gmail.com>
2020-04-15 14:11:12 +0200

    [feat] adding a visitedPlacesCard component

--- lib/views/places/placesState.dart
+++ lib/views/places/placesState.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pandemia/components/visitedPlacesCard.dart';
 import 'package:pandemia/views/places/places.dart';
 
 class PlacesState extends State<PlacesView> {
   GoogleMapController mapController;
-
+  String _mapStyle;
   final LatLng _center = const LatLng(50.6311652, 3.0477402);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    controller.setMapStyle(_mapStyle);
   }
 
   @override
   Widget build(BuildContext context) {
+    rootBundle.loadString('assets/mapstyle.txt').then((string) {
+      _mapStyle = string;
+    });
     return MaterialApp(
       home: Scaffold(
         body: GoogleMap(

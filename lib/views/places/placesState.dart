54d045f7d6f1aabdb3dbae6f0edb260c3171a0ce
Rémy Raes <raes.remy@gmail.com>
2020-04-15 11:25:57 +0200

    [feat] adding GMaps API key for Android + places layout

---
+++ lib/views/places/placesState.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pandemia/views/places/places.dart';
+
+class PlacesState extends State<PlacesView> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(50.6311652, 3.0477402);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 13.75,
+          ),
+        ),
+      ),
+    );
+  }
+}

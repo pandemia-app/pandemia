f9fd978fabaadd43ea090c1195c2cef5b4dbf19f
Rémy Raes <raes.remy@gmail.com>
2020-04-30 11:29:48 +0200

    [fix] moving location listener outside of build method

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,16 @@ final AppDatabase db = new AppDatabase();
 
 void main() async {
   await DotEnv().load('lib/.env.generated');
+  db.open();
+
+  // subscribing to location updates
+  geolocator.getPositionStream(locationOptions).listen((Position position) {
+    if (position == null) return;
+    var loc = new Location(id: 0, lat: position.latitude, lng: position.longitude,
+        timestamp: position.timestamp);
+    db.insertLocation(loc).then((_) => print('new location stored: $loc'));
+  });
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppModel(),
@@ -25,14 +35,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    db.open();
-    geolocator.getPositionStream(locationOptions).listen((Position position) {
-      if (position == null) return;
-      var loc = new Location(id: 0, lat: position.latitude, lng: position.longitude,
-          timestamp: position.timestamp);
-      db.insertLocation(loc).then((_) => print('new location stored: $loc'));
-    });
-
     return MaterialApp(
       title: 'Pandemia',
       home: MyHomePage(),
@@ -42,15 +44,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   BottomNavigationWidgetState createState() => BottomNavigationWidgetState();

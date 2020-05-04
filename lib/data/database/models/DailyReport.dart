661586de21640389eb9d1e16108654063103d8de
Alystrasz <raes.remy@gmail.com>
2020-05-04 10:54:28 +0200

    [feat] adding broadcastRate to db entity

--- lib/data/database/models/DailyReport.dart
+++ lib/data/database/models/DailyReport.dart
@@ -1,6 +1,7 @@
 class DailyReport {
   final int expositionRate;
+  final int broadcastRate;
   final DateTime timestamp;
 
-  DailyReport ({this.expositionRate, this.timestamp});
+  DailyReport ({this.expositionRate, this.broadcastRate, this.timestamp});
 }

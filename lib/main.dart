e76c90db1629c7b86f7987b9efbbf21f3ea47bc4
Rémy Raes <raes.remy@gmail.com>
2020-05-04 21:22:28 +0200

    [refactor] adding translations for bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:pandemia/navigator.dart';
 import 'package:provider/provider.dart';
 import 'data/state/AppModel.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   await DotEnv().load('lib/.env.generated');
@@ -18,9 +21,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    initializeDateFormatting();
+
     return MaterialApp(
       title: 'Pandemia',
       home: MyHomePage(),
+      localizationsDelegates: [
+        FlutterI18nDelegate(
+          translationLoader: FileTranslationLoader(
+            useCountryCode: false
+          )
+        )
+      ],
     );
   }
 }

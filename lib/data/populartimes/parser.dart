d6098c8d7fecba357483df16d0126cd0070f2d15
Alystrasz <raes.remy@gmail.com>
2020-05-13 15:58:54 +0200

    [chore] adding html dependency

--- lib/data/populartimes/parser.dart
+++ lib/data/populartimes/parser.dart
@@ -1,3 +1,4 @@
+import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 /// https://stackoverflow.com/questions/30857150/getting-google-maps-link-from-place-id
@@ -9,13 +10,15 @@ class Parser {
 
   static Future<String> getHTML (String placeId) async {
     String url = "https://www.google.com/maps/place/?q=place_id:$placeId";
+    print(url);
     var response = await http.get(url);
-    print(response.statusCode);
-    print(response.body);
-    return "";
+    return response.body;
   }
 
   static int parseHTML(String html) {
+    var document = parse(html);
+    var tags = document.querySelectorAll('[aria-label]');
+    print(tags.length);
     return 0;
   }
 }

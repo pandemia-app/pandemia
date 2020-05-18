9eeaa257c0605d727cb10dcba6f60f8a97613b28
Alystrasz <raes.remy@gmail.com>
2020-05-18 22:59:30 +0200

    [test] adding some tests for popular times parser

---
+++ test/parser_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pandemia/data/database/models/Favorite.dart';
+import 'package:pandemia/data/populartimes/parser.dart';
+
+// TODO provide an HTTPClient implementation (http calls are not allowed in tests)
+void main() {
+  testWidgets('Testing popular times parser', (WidgetTester tester) async {
+    Favorite placeWithStats = new Favorite(
+        name: "E.Leclerc Paris 19", address: "191 Boulevard Macdonald, 75019 Paris, France");
+    Favorite placeWithoutStats = new Favorite(
+      name: "Lilliad", address: "2, Avenue Jean Perrin, 59650 Villeneuve-d'Ascq, France"
+    );
+
+    print('Checking popular times of a place that have some');
+    var stats = await Parser.getPopularTimes(placeWithStats);
+    expect(stats.length, 18);
+    expect(stats[0][0], 6);
+    expect(stats[17][0], 23);
+
+    print('Checking popular times of a place that does not have some');
+    var result = await Parser.getPopularTimes(placeWithoutStats);
+    expect(result, -1);
+  });
+}

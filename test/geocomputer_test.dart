b0e7a827b9b292a1143096a11e414e4bf1b87683
Alystrasz <raes.remy@gmail.com>
2020-06-02 17:56:34 +0200

    [test] adding tests for search radius computing

--- test/geocomputer_test.dart
+++ test/geocomputer_test.dart
@@ -8,7 +8,7 @@ void main() {
     computer = new GeoComputer();
   });
 
-  group('Geocomputer tests', () {
+  group('getBoundsCenterLocation()', () {
     test('should return square centre location', () {
       LatLngBounds bounds = new LatLngBounds(southwest: LatLng(2, 4), northeast: LatLng(4, 2));
       LatLng center = computer.getBoundsCenterLocation(bounds);
@@ -31,6 +31,52 @@ void main() {
       LatLngBounds bounds = new LatLngBounds(southwest: LatLng(7, 7), northeast: LatLng(7, 7));
       LatLng center = computer.getBoundsCenterLocation(bounds);
       expect(center, LatLng(7, 7));
+    });
+  });
+
+  group('Search radius computing', () {
+    test('should return 0 for non-existent zone', () {
+      LatLngBounds bounds = new LatLngBounds(southwest: LatLng(42, 3), northeast: LatLng(42, 3));
+      double radius = computer.getSearchRadius(bounds);
+      expect(radius, 0.0);
+    });
+
+    // the following tests use pre-computed values to ensure order of returned
+    // values is coherent
+    test('should return a radius for a portrait-oriented zone', () {
+      LatLngBounds bounds = new LatLngBounds(
+        northeast: LatLng(50.486364, 3.386926),
+        southwest: LatLng(50.401621, 3.286566)
+      );
+      double radius = computer.getSearchRadius(bounds);
+      expect(radius, 4713);
+    });
+
+    test('should return a radius for a landscape-oriented zone', () {
+      LatLngBounds bounds = new LatLngBounds(
+          northeast: LatLng(50.239105, 4.345443),
+          southwest: LatLng(49.628964, 1.061071)
+      );
+      double radius = computer.getSearchRadius(bounds);
+      expect(radius, 117896);
+    });
+
+    test('should return a radius with bounds including negative longitude', () {
+      LatLngBounds bounds = new LatLngBounds(
+          northeast: LatLng(50.595359, 0.104460),
+          southwest: LatLng(50.550336, -0.076399)
+      );
+      double radius = computer.getSearchRadius(bounds);
+      expect(radius, 6406);
+    });
+
+    test('should return a radius with bounds including negative latitude', () {
+      LatLngBounds bounds = new LatLngBounds(
+          northeast: LatLng(0.485556, 23.143687),
+          southwest: LatLng(-0.449652, 18.868404)
+      );
+      double radius = computer.getSearchRadius(bounds);
+      expect(radius, 237961);
     });
   });
 }

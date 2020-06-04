bea59965067b9bdf9bfcd545b3e8838c2e29c391
Alystrasz <raes.remy@gmail.com>
2020-06-04 16:04:58 +0200

    [refactor] exporting information sheet to new dedicated component

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,7 +5,7 @@ import 'package:pandemia/components/home/expositionProgressionCard.dart';
 import 'package:pandemia/components/home/myExpositionCard.dart';
 import 'package:pandemia/components/home/visitedPlacesCard.dart';
 import 'package:pandemia/utils/CustomPalette.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'file:///D:/workspace/pandemia/lib/utils/information/InformationSheet.dart';
 import 'package:pandemia/data/database/indicatorsComputer.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -33,6 +33,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     // launching analysis
     computer.generateRandomReport(context);
+    InformationSheet sheet = new InformationSheet(context);
 
     return SafeArea (
       child: Scaffold(
@@ -40,7 +41,7 @@ class HomeView extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.info_outline),
           onPressed: () {
-            _settingModalBottomSheet(context);
+            sheet.show();
           },
         ),
         body: SmartRefresher(
@@ -63,98 +64,5 @@ class HomeView extends StatelessWidget {
         )
       )
     );
-  }
-
-  /// Displays a text sheet displaying information about the application.
-  void _settingModalBottomSheet (context) {
-    var padding = EdgeInsets.symmetric(horizontal: 20, vertical: 10);
-    var value = true;
-
-    showModalBottomSheet(
-        context: context,
-        isScrollControlled: true,
-        builder: (BuildContext bc) {
-          return Container(
-            child: new Wrap(
-              children: <Widget>[
-                new ListTile(
-                    leading: new Icon(Icons.info_outline),
-                    title: new Text(
-                      FlutterI18n.translate(context, "home_info_operation_title"),
-                    ),
-                    contentPadding: padding,
-                    subtitle: Text(
-                      FlutterI18n.translate(context, "home_info_operation_text1") + '\n' +
-                          FlutterI18n.translate(context, "home_info_operation_text2"),
-                    ),
-                ),
-                new ListTile(
-                  leading: new Icon(Icons.data_usage),
-                  contentPadding: padding,
-                  title: new Text(
-                      FlutterI18n.translate(context, "home_info_data_title")
-                  ),
-                  subtitle: Text(
-                    FlutterI18n.translate(context, "home_info_data_text1")
-                  ),
-                ),
-                new ListTile(
-                  leading: new Icon(Icons.location_on),
-                  contentPadding: padding,
-                  title: new Text(
-                      FlutterI18n.translate(context, "home_info_location_title")
-                  ),
-                  subtitle: Text(
-                      FlutterI18n.translate(context, "home_info_location_text")
-                  ),
-                  trailing: Switch(value: value, onChanged: (bool newValue) {
-                    value = newValue;
-                  }),
-                ),
-                new ListTile(
-                  leading: new Icon(Icons.warning),
-                  contentPadding: padding,
-                  title: new Text(
-                      FlutterI18n.translate(context, "home_info_disclaimer_title")
-                  ),
-                  subtitle: Text(
-                      FlutterI18n.translate(context, "home_info_disclaimer_text")
-                  ),
-                ),
-                new ListTile(
-                  onTap: _openRepositoryURL,
-                  leading: new Icon(Icons.code),
-                  contentPadding: padding,
-                  title: new Text(
-                      FlutterI18n.translate(context, "home_info_source_title")
-                  ),
-                  subtitle: Text.rich(
-                    TextSpan(
-                      text: FlutterI18n.translate(context, "home_info_source_text") + " ",
-                      children: <TextSpan>[
-                      TextSpan(
-                        text: 'https://github.com/pandemia-app/pandemia',
-                        style: TextStyle(
-                        decoration: TextDecoration.underline,
-                      )),
-                      ],
-                    )
-                  ),
-                ),
-              ],
-            ),
-          );
-        }
-    );
-  }
-
-  /// Redirects the user to the application's GitHub repository on the web.
-  _openRepositoryURL () async {
-    const url = 'https://github.com/pandemia-app/pandemia';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
   }
 }

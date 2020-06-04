e2ac355538ce0fe7fc1b163eefc9fbe9a308b8d4
Alystrasz <raes.remy@gmail.com>
2020-06-05 01:17:06 +0200

    [feat] displaying a message when trying to allow location
    When asking for the location permission, a user might have selected
    the "do not ask me again" option; we display a toast to warn him,
    and then redirects him to the settings screen, where the location
    permission can be updated.

--- lib/utils/information/GeoTile.dart
+++ lib/utils/information/GeoTile.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 /// List tile allowing the user to update its location policy.
@@ -13,10 +16,11 @@ class GeoTile extends StatefulWidget {
 
 class GeoTileState extends State<GeoTile> {
   bool _value = false;
+  BuildContext _context;
 
   @override
   void initState() {
-    Permission.location.status.then((value) {
+    Permission.locationAlways.status.then((value) {
       setState(() {
         _value = value.isGranted;
       });
@@ -31,10 +35,30 @@ class GeoTileState extends State<GeoTile> {
     _checkPermissions();
   }
 
-  void _checkPermissions () async { }
+  void _checkPermissions () async {
+    if (_value) {
+      var permissionRequest = await Permission.locationAlways.request();
+      if (!permissionRequest.isGranted) {
+        if (permissionRequest.isPermanentlyDenied) {
+          Fluttertoast.showToast(
+              msg: FlutterI18n.translate(_context, "home_info_location_permanentlydenied"),
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 4,
+              fontSize: 16.0
+          );
+          Timer(Duration(seconds: 2), () => openAppSettings());
+        }
+        setState(() {
+          _value = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    _context = context;
     return ListTile(
       onTap: () => _setNewValue(!_value),
       leading: new Icon(Icons.location_on),

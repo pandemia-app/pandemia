45c8330f903324fd6dd51e882fe227d6b2431f49
Alystrasz <raes.remy@gmail.com>
2020-06-04 23:49:32 +0200

    [feat] switch shows state of the location permission

--- lib/utils/information/GeoSwitch.dart
+++ lib/utils/information/GeoSwitch.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class GeoSwitch extends StatefulWidget {
   final GeoSwitchState _state = GeoSwitchState();
   @override
   State<StatefulWidget> createState() {
+    _state.initSwitchState();
     return _state;
   }
 
@@ -15,6 +17,13 @@ class GeoSwitch extends StatefulWidget {
 
 class GeoSwitchState extends State<GeoSwitch> {
   bool _value = false;
+
+  void initSwitchState () async {
+    var status = await Permission.location.status;
+    setState(() {
+      _value = status.isGranted;
+    });
+  }
 
   void toggle () {
     _setNewValue(!_value);
@@ -27,9 +36,7 @@ class GeoSwitchState extends State<GeoSwitch> {
     _checkPermissions();
   }
 
-  void _checkPermissions () {
-
-  }
+  void _checkPermissions () async { }
 
   @override
   Widget build(BuildContext context) {

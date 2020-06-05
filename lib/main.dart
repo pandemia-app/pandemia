c74ce42c2c47c38b76624462585ecddfcd97eca2
Alystrasz <raes.remy@gmail.com>
2020-06-05 13:36:19 +0200

    [feat] displaying a message when location permission has been granted but location service is not running

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     initializeDateFormatting();
--- lib/navigator.dart
+++ lib/navigator.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pandemia/utils/CustomPalette.dart';
 import 'package:pandemia/views/favorites/view.dart';
 import 'package:pandemia/views/home.dart';
 import 'package:pandemia/views/places/places.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'data/state/AppModel.dart';
 import 'main.dart';
@@ -31,6 +33,22 @@ class BottomNavigationWidgetState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // checking location service status if permission has been granted
+    Permission.locationAlways.isGranted.then((value) {
+      if (!value) return;
+      Permission.locationAlways.serviceStatus.then((value) {
+        if (value.isDisabled) {
+          Fluttertoast.showToast(
+              msg: FlutterI18n.translate(context, "location_service_disabled"),
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 4,
+              fontSize: 16.0
+          );
+        }
+      });
+    });
+
     return Consumer<AppModel>(
         builder: (context, model, child) {
           return Scaffold(

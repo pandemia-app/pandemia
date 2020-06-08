ec7516e7553d5254741dd2f16d0c91e6fdf45307
Alystrasz <raes.remy@gmail.com>
2020-06-08 14:48:05 +0200

    [chore] removing useless imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:background_locator/background_locator.dart';
-import 'package:background_locator/location_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:pandemia/data/database/database.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';

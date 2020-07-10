fc6916cb84a972c02c4917b7853c7c10a6651a01
Alystrasz <raes.remy@gmail.com>
2020-07-10 18:24:19 +0200

    [fix] removing useless code

--- lib/utils/charts/barChart.dart
+++ lib/utils/charts/barChart.dart
@@ -1,5 +1,4 @@
 import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:pandemia/utils/CustomPalette.dart';
 

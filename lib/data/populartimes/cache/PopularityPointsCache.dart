2ed4b96f4b29f4b559493427fc7309da43229000
Alystrasz <raes.remy@gmail.com>
2020-07-20 13:36:59 +0200

    [docs] adding documentation for the popularity points cache

--- lib/data/populartimes/cache/PopularityPointsCache.dart
+++ lib/data/populartimes/cache/PopularityPointsCache.dart
@@ -1,11 +1,14 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pandemia/utils/GeoComputer.dart';
 
+/// Stores points representing the current popularity of a given place.
 class PopularityPointsCache {
   Map<String, Map<String, WeightedLatLng>> points = new Map();
   Map<String, double> zoomLevels = new Map();
   GeoComputer computer = new GeoComputer();
 
+  /// Returns points for a given place.
+  /// Regenerates points if the zoom level has been changed since last request.
   Map<String, WeightedLatLng> getPoints (String placeId, LatLng placePosition, int placeCurrentPopularity, double zoomLevel) {
     if (this.points[placeId] == null || this.zoomLevels[placeId] != zoomLevel) {
       points[placeId] = computer.generatePopularityPoints(placePosition, placeId, placeCurrentPopularity, zoomLevel);

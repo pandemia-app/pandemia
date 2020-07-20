784bc333f15eadd74ca9942707161ca7dad96dee
Alystrasz <raes.remy@gmail.com>
2020-07-20 13:25:11 +0200

    [feat] recomputing popularity points if zoom level is different

--- lib/data/populartimes/cache/PopularityPointsCache.dart
+++ lib/data/populartimes/cache/PopularityPointsCache.dart
@@ -3,12 +3,16 @@ import 'package:pandemia/utils/GeoComputer.dart';
 
 class PopularityPointsCache {
   Map<String, Map<String, WeightedLatLng>> points = new Map();
+  Map<String, double> zoomLevels = new Map();
   GeoComputer computer = new GeoComputer();
 
   Map<String, WeightedLatLng> getPoints (String placeId, LatLng placePosition, int placeCurrentPopularity, double zoomLevel) {
-    if (this.points[placeId] == null) {
+    if (this.points[placeId] == null || this.zoomLevels[placeId] != zoomLevel) {
       points[placeId] = computer.generatePopularityPoints(placePosition, placeId, placeCurrentPopularity, zoomLevel);
     }
+
+    zoomLevels[placeId] = zoomLevel;
+
     return points[placeId];
   }
 }

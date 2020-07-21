12c887ce367336260c74aed68754b58f94a7f1e1
Rémy Raes <raes.remy@gmail.com>
2020-07-21 16:43:17 +0200

    [fix] iOS app does not crash at start anymore

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,13 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import background_locator
+
+func registerPlugins(registry: FlutterPluginRegistry) -> () {
+    if (!registry.hasPlugin("BackgroundLocatorPlugin")) {
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+}
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +17,7 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyAX8bDIMgseRdGxcv5XNGeU8n9mhwLWMVk")
     GeneratedPluginRegistrant.register(with: self)
+    BackgroundLocatorPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }

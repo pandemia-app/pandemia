ccf7073e9e4cba9c44b0523ba37aed07af00baed
Alystrasz <raes.remy@gmail.com>
2020-09-21 10:51:57 +0200

    [test] checking circular search zone inputs

--- test/utils/placesMap/search_zone_test.dart
+++ test/utils/placesMap/search_zone_test.dart
@@ -3,11 +3,30 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pandemia/utils/placesMap/SearchZone.dart';
 
 void main() {
+  CircularSearchZone _zone;
   test("should create a valid search zone", () {
-    CircularSearchZone zone = CircularSearchZone(
+    _zone = CircularSearchZone(
       center: LatLng(55.17, 3.14),
       radius: 5
     );
-    expect(zone.isValid(), true);
+    expect(_zone.isValid(), true);
+  });
+
+  test("cannot be created with null parameters", () {
+    expect(() {
+      _zone = CircularSearchZone(center: null, radius: null);
+    }, throwsAssertionError);
+  });
+
+  test("cannot be created with null center", () {
+    expect(() {
+      _zone = CircularSearchZone(center: null, radius: 42);
+    }, throwsAssertionError);
+  });
+
+  test("cannot be created with null radius", () {
+    expect(() {
+      _zone = CircularSearchZone(center: LatLng(55.17, 3.14), radius: null);
+    }, throwsAssertionError);
   });
 }

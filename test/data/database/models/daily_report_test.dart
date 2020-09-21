22e697b02d0c758a9306bfb9eea437e381c91964
Alystrasz <raes.remy@gmail.com>
2020-09-21 15:03:58 +0200

    [refactor] removing useless code from tests

--- test/data/database/models/daily_report_test.dart
+++ test/data/database/models/daily_report_test.dart
@@ -4,42 +4,36 @@ import 'package:pandemia/data/database/models/DailyReport.dart';
 void main () {
   test("should not accept null exposition rate", () {
     expect(() {
-      final DailyReport report =
         DailyReport(expositionRate: null, broadcastRate: 45, timestamp: 1);
     }, throwsAssertionError);
   });
 
   test("should not accept negative exposition rate", () {
     expect(() {
-      final DailyReport report =
       DailyReport(expositionRate: -1, broadcastRate: 45, timestamp: 1);
     }, throwsAssertionError);
   });
 
   test("should not accept null broadcast rate", () {
     expect(() {
-      final DailyReport report =
       DailyReport(expositionRate: 45, broadcastRate: null, timestamp: 1);
     }, throwsAssertionError);
   });
 
   test("should not accept negative broadcast rate", () {
     expect(() {
-      final DailyReport report =
       DailyReport(expositionRate: 45, broadcastRate: -1, timestamp: 1);
     }, throwsAssertionError);
   });
 
   test("should not accept null timestamp", () {
     expect(() {
-      final DailyReport report =
       DailyReport(expositionRate: 45, broadcastRate: 42, timestamp: null);
     }, throwsAssertionError);
   });
 
   test("should not accept negative timestamp", () {
     expect(() {
-      final DailyReport report =
       DailyReport(expositionRate: 45, broadcastRate: 42, timestamp: -5455286);
     }, throwsAssertionError);
   });

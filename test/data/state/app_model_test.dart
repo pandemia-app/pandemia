c97c818adcb2dd856d55aef29396bc7e0355777f
Alystrasz <raes.remy@gmail.com>
2020-09-21 12:24:22 +0200

    [test] trying to set AppModel's tab index

--- test/data/state/app_model_test.dart
+++ test/data/state/app_model_test.dart
@@ -3,10 +3,31 @@ import 'package:pandemia/data/state/AppModel.dart';
 
 void main() {
   AppModel _model;
+  setUp(() {
+    _model = AppModel();
+  });
 
   test("should be initialised with default values", () {
-    _model = AppModel();
     expect(_model.tabIndex, 0);
     expect(_model.reports, []);
+  });
+
+  test("should set tab index", () {
+    expect(_model.tabIndex, 0);
+    _model.setTabIndex(2);
+    expect(_model.tabIndex, 2);
+  });
+
+  // there are only 3 tabs, index varies from 0 to 2 included
+  test("should not set tab index", () {
+    expect(() {
+      _model.setTabIndex(3);
+    }, throwsAssertionError);
+  });
+
+  test("should not set negative tab index", () {
+    expect(() {
+      _model.setTabIndex(-1);
+    }, throwsAssertionError);
   });
 }

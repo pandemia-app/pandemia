f85192d4898829720c497a67821d4340b437684a
Alystrasz <raes.remy@gmail.com>
2020-09-21 18:45:32 +0200

    [docs] adding coverage HTML pages

--- tool/run_tests.sh
+++ tool/run_tests.sh
@@ -6,4 +6,7 @@ flutter test --coverage test
 echo "Generating coverage badge..."
 flutter pub run flutter_coverage_badge
 
+echo "Generating HTML pages..."
+genhtml coverage/lcov.info -o coverage/html
+
 echo "Done!"

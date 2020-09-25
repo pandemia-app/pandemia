52e4cca418b8602f78d0ced8288212e0fc47e2ab
Alystrasz <raes.remy@gmail.com>
2020-09-25 11:40:08 +0200

    [test] checking PopularTimes.toString

--- test/populartimes/payloads/populartimes_test.dart
+++ test/populartimes/payloads/populartimes_test.dart
@@ -21,6 +21,12 @@ void main() {
     }, throwsAssertionError);
   });
 
+  test("instantiation is not possible when popularity is null", () {
+    expect(() {
+      PopularTimes(stats: Map(), currentPopularity: null);
+    }, throwsAssertionError);
+  });
+
   test("instantiation is not possible when providing popularity only", () {
     expect(() {
       PopularTimes(currentPopularity: 42);
@@ -65,5 +71,20 @@ void main() {
     expect(() {
       _times.getOrderedKeys();
     }, throwsAssertionError);
+  });
+
+  test("should convert to string", () {
+    _times = PopularTimes(currentPopularity: 42, stats: {
+      7: DayResult(),
+      1: DayResult(),
+      2: DayResult(),
+      3: DayResult(),
+      4: DayResult(),
+      5: DayResult(),
+      6: DayResult()
+    });
+
+    String expected = "PopularTimes[hasData=true, currentPopularity=42, stats={7: Instance of 'DayResult', 1: Instance of 'DayResult', 2: Instance of 'DayResult', 3: Instance of 'DayResult', 4: Instance of 'DayResult', 5: Instance of 'DayResult', 6: Instance of 'DayResult'}]";
+    expect(_times.toString(), expected);
   });
 }

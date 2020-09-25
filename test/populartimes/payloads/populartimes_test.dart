37b462fdf9ba5d22e724f127d43424bf372c9092
Alystrasz <raes.remy@gmail.com>
2020-09-25 11:26:56 +0200

    [test] PopularTimes is correctly instantiable

--- test/populartimes/payloads/populartimes_test.dart
+++ test/populartimes/payloads/populartimes_test.dart
@@ -1,4 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pandemia/data/populartimes/payloads/populartimes.dart';
 
-void main() {}
+void main() {
+  PopularTimes _times;
+
+  test("instance should not have data", () {
+    _times = PopularTimes();
+    expect(_times.hasData, false);
+  });
+
+  test("instance should have data", () {
+    _times = PopularTimes(currentPopularity: 42, stats: Map());
+    expect(_times.hasData, true);
+  });
+
+  test("instantiation is not possible when providing stats only", () {
+    expect(() {
+      PopularTimes(stats: Map());
+    }, throwsAssertionError);
+  });
+
+  test("instantiation is not possible when providing popularity only", () {
+    expect(() {
+      PopularTimes(currentPopularity: 42);
+    }, throwsAssertionError);
+  });
+}

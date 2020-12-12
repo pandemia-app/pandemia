0666bd622d8035b1f2cf7656e9b97b16493c197d
FXpel <fxpel@DESKTOP-2NFFNN9.localdomain>
2020-12-12 18:32:24 +0100

    fix(geoloc bdd): accès à la bdd lors de l'enregistrement ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,24 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:pandemia/data/state/MapModel.dart';
 import 'package:pandemia/views/home/navigator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:pandemia/utils/geolocation/Geolocator.dart';
 import 'package:provider/provider.dart';
 import 'data/state/AppModel.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   await DotEnv().load('lib/.env.generated');
+  // checking location permission + status before launching location gathering
+  Permission.locationAlways.status.then((permissionStatus) {
+    if (permissionStatus.isGranted) {
+      Permission.locationAlways.serviceStatus.then((serviceStatus) {
+        if (serviceStatus.isEnabled) {
+          Geolocator.launch();
+        }
+      });
+    }
+  });
   runApp(
       MultiProvider (
         providers: [

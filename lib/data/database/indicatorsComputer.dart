f519bfed0f8fbb2ed37312ef218897cf1648f163
Rémy Raes <raes.remy@gmail.com>
2021-03-23 11:22:15 +0100

    [fix] reimporting markers computation code

--- lib/data/database/indicatorsComputer.dart
+++ lib/data/database/indicatorsComputer.dart
@@ -1,9 +1,13 @@
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:pandemia/data/database/database.dart';
 import 'package:pandemia/data/database/models/DailyReport.dart';
 import 'package:pandemia/data/state/AppModel.dart';
 import 'package:provider/provider.dart';
+import 'package:pandemia/data/database/models/Location.dart' as L;
+
+import 'models/dataCollect.dart';
 var database = new AppDatabase();
 
 /// This is responsible for generating daily pandemia reports.
@@ -12,19 +16,30 @@ class IndicatorsComputer {
   // we need to be able to block further calls
   var generated = false;
 
+  // TODO refactor
+  void lieu() async{
+    List<L.Location> liste = await database.getLocations();
+    for (L.Location loc in liste){
+      await Geolocator().placemarkFromCoordinates(loc.lat, loc.lng);
+    }
+  }
+
   /// is called several times a day to update today's report
   /// returns the exposition rate of the day
   Future<void> generateRandomReport (BuildContext context) async {
     if (generated) return;
     print('generating report');
 
-    // TODO rates computing
+    lieu();
     await new Future.delayed(const Duration(milliseconds: 750), () {});
 
+    print("-------------------------------");
+    print(DataCollect.result);
+    var result = DataCollect.result <100 ? DataCollect.result.round() : 100;
     var report = new DailyReport(
         timestamp: DailyReport.getTodaysTimestamp(),
         broadcastRate: new Random().nextInt(100),
-        expositionRate: new Random().nextInt(100)
+        expositionRate: result
     );
     await setTodaysReport(report);
 
@@ -34,6 +49,7 @@ class IndicatorsComputer {
 
     generated = true;
   }
+
 
   Future<void> forceReportRecomputing (BuildContext context) async {
     generated = false;

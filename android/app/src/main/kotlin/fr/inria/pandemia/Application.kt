d3f195dd94562cdde7f06522fa1ecb8ccf572984
Rémy Raes <raes.remy@gmail.com>
2021-03-23 11:58:55 +0100

    [fix] registering Java GeolocatorPlugin to use it while in background

--- android/app/src/main/kotlin/fr/inria/pandemia/Application.kt
+++ android/app/src/main/kotlin/fr/inria/pandemia/Application.kt
@@ -5,12 +5,12 @@ import io.flutter.plugin.common.PluginRegistry
 import io.flutter.view.FlutterMain
 import rekab.app.background_locator.IsolateHolderService
 import com.tekartik.sqflite.SqflitePlugin
+import com.baseflow.geolocator.GeolocatorPlugin
 
 class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
         IsolateHolderService.setPluginRegistrant(this)
-        // LocatorService.setPluginRegistrant(this)
         FlutterMain.startInitialization(this)
     }
 
@@ -18,5 +18,7 @@ class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallbac
         if (!registry!!.hasPlugin("com.tekartik.sqflite")) {
             SqflitePlugin.registerWith(registry.registrarFor("com.tekartik.sqflite"))
         }
+        if (!registry!!.hasPlugin("com.baseflow.geolocator"))
+            GeolocatorPlugin.registerWith(registry.registrarFor("com.baseflow.geolocator"))
     }
 }

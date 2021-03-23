a3c67d2b380aef10397503994c290a6c230a579c
Rémy Raes <raes.remy@gmail.com>
2021-03-23 15:29:17 +0100

    [feat] registering background plugins for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,9 @@ import Flutter
 import GoogleMaps
 import Firebase
 import background_locator
+import sqflite
+import geolocator
+import flutter_local_notifications
 
 func registerPlugins(registry: FlutterPluginRegistry) -> () {
     if (!registry.hasPlugin("BackgroundLocatorPlugin")) {
@@ -20,6 +23,22 @@ func registerPlugins(registry: FlutterPluginRegistry) -> () {
     GMSServices.provideAPIKey("AIzaSyAX8bDIMgseRdGxcv5XNGeU8n9mhwLWMVk")
     GeneratedPluginRegistrant.register(with: self)
     BackgroundLocatorPlugin.setPluginRegistrantCallback(registerPlugins)
+    
+    if !hasPlugin("com.tekartik.sqflite") {
+        SqflitePlugin
+            .register(with: registrar(forPlugin: "com.tekartik.sqflite")!)
+    }
+    
+    if !hasPlugin("com.baseflow.geolocator") {
+        GeolocatorPlugin
+            .register(with: registrar(forPlugin: "com.baseflow.geolocator")!)
+    }
+    
+    if !hasPlugin("com.dexterous.flutterlocalnotifications") {
+        FlutterLocalNotificationsPlugin
+            .register(with: registrar(forPlugin: "com.dexterous.flutterlocalnotifications")!)
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
